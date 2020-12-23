0da78e50eda5a8ed5b43c42ce36d7c78d1ff9d45
javahongxi <javahongxi@qq.com>
2020-12-24 04:31:40 +0800

    oop

--- lang/oop/student.py
+++ lang/oop/student.py
@@ -7,17 +7,35 @@ class Student():
     # self 名字任意
     def __init__(self, name, age):
         # 实例变量
-        self.name = name
+        self.name = name # public
         self.age = age
+        self.__score = 0 # private
         # 构造函数中访问类变量
         self.__class__.total += 1
 
     # 实例方法
     def to_str(self):
-        print(self.name, self.age)
+        print(self.name, self.age, self.__score)
+
+    # 实例变量建议私有，通过提供方法来修改值
+    def marking(self, score):
+        self.__p_method()
+        self.print_total()
+        if score < 0:
+            return 'negative score!!!'
+        self.__score = score
+
+    # 私有方法
+    def __p_method(self):
+        print('private method', self.__hash__())
 
     # 类方法
     @classmethod
     def print_total(cls):
         # 实例方法中访问类变量
         print('total:', cls.total)
+
+    # 静态方法
+    @staticmethod
+    def print_total2():
+        print('total:', Student.total)
--- lang/oop/test.py
+++ lang/oop/test.py
@@ -1,11 +1,27 @@
 from student import Student
 
 student = Student('Lily', 24)
-student.to_str()
 print(student.name)
 print(student.age)
+student.to_str()
 
-print('total:', str(Student.total))
 Student.print_total()
 
+Student.print_total2()
+
 print(student.__dict__)
+# {'name': 'Lily', 'age': 24, '_Student__score': 0}
+# print(student._Student__score)
+
+result = student.marking(80)
+if result:
+    print(result)
+
+student.to_str()
+
+lucy = Student('Lucy', 24)
+lee = Student('Lee', 25)
+# 这里实际上是新创建一个实例变量，动态语言特性
+lucy.__score = -1
+# print(lucy.__score)
+lucy.to_str()
