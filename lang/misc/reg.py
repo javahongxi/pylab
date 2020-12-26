05df4a82ca619aec629e3a3e864598bdf59e4504
javahongxi <javahongxi@qq.com>
2020-12-26 08:14:34 +0800

    misc

--- lang/misc/reg.py
+++ lang/misc/reg.py
@@ -26,3 +26,59 @@ print(r)
 s = 'acc, abc, acd, adf, ccd, afc'
 r = re.findall('a[cf]c', s)
 print(r)
+
+s = 'python 111java678php'
+r = re.findall('[a-z]{3,6}', s) # 贪婪
+print(r)
+r = re.findall('[a-z]{3,6}?', s) # 非贪婪
+print(r)
+
+s = 'pytho0python1pythonn2'
+r = re.findall('python*', s)
+print(r)
+r = re.findall('python+', s)
+print(r)
+r = re.findall('python?', s)
+print(r)
+
+s = '100001'
+r = re.findall('^\\d{4,8}$', s)
+print(r)
+
+s = 'PythonPythonPythonPythonPython'
+r = re.findall('(Python){3}', s)
+print(r)
+
+s = 'PythonC#\nJavaPHP'
+r = re.findall('c#.{1}', s, re.I | re.S)
+print(r)
+
+s = 'PythonC#JavaPHPC#'
+r = re.sub('C#', 'GO', s, 0)
+print(r)
+s = s.replace('C#', 'GO')
+print(s)
+
+
+# 将匹配的数字乘于 2
+def double(matched):
+    value = int(matched.group('value'))
+    return str(value * 2)
+
+
+s = 'A23G4HFD567'
+print(re.sub('(?P<value>\\d+)', double, s))
+
+print(re.match('\\d', s)) # 从首字母开始匹配
+r = re.search('\\d', s)
+print(r)
+print(r.span())
+print(r.group())
+
+s = 'life is short, i use python, i love python'
+r = re.search('life(.*)python(.*)python', s)
+print(r.group(0))
+print(r.group(1))
+print(r.group(2))
+print(r.group(1, 2))
+print(r.groups())
